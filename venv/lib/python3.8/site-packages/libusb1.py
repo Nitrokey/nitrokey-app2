edc122b9b64e6a1d20f557aef28a03318fe1b4c1
Niklas <64582081+Niklas-Nitrokey@users.noreply.github.com>
2022-05-18 22:24:38 +0200

    Delete venv directory

--- venv/lib/python3.8/site-packages/libusb1.py
+++
@@ -1,8 +0,0 @@
-import warnings
-warnings.warn(
-  'Importing this module should not be needed, everything intended to be '
-  'exposed should be available in usb1 module.',
-  DeprecationWarning,
-)
-del warnings
-from usb1.libusb1 import *
