7a231009e9e3415e4d170ee90a6c8c15aeb467c7
Markus Merklinger <markus@nitrokey.com>
2023-02-09 15:47:03 +0100

    Include version file into init module

--- nitrokeyapp/__init__.py
+++ nitrokeyapp/__init__.py
@@ -1,4 +1,6 @@
 """Experimental Nitrokey GUI Application - based on pynitrokey"""
 
+import pathlib
 
-__version__ = "0.0.1"
+__version_path__ = pathlib.Path(__file__).parent.resolve().absolute() / "VERSION"
+__version__ = open(__version_path__).read().strip()
