ddc94b9fb67757f72d0ffc27fa78c4a96fc912ae
Markus Merklinger <markus@nitrokey.com>
2023-07-03 11:41:57 +0200

    Remove unused UI adaption script

--- nitrokeyapp/ui/adapt_ui_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp bak.org/resources.qrc .
-cp -r bak.org/*.ui .
-
-
-sed -i -e 's@../../resources.qrc@resources.qrc@g' *.ui
-
-#sed -i -e 's@<file>images/@<file>images/@g' resources.qrc
