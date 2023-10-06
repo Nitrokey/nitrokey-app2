bfc59108b0829f47215cc8f377060f77fd387ec6
jj-so <janos@nitrokey.com>
2023-10-06 16:20:40 +0200

    add AppUpdateCheck 	modified:   nitrokeyapp/ui/welcome_tab.ui 	modified:   nitrokeyapp/welcome_tab.py
    center version info
    	modified:   nitrokeyapp/ui/mainwindow.ui
    	modified:   nitrokeyapp/ui/welcome_tab.ui
    
        pick 320769a add AppUpdateCheck 	modified:   nitrokeyapp/ui/welcome_tab.ui 	modified:   nitrokeyapp/welcome_tab.py
        squash 54607ae center version info 	modified:   nitrokeyapp/ui/mainwindow.ui 	modified:   nitrokeyapp/ui/welcome_tab.ui
    	modified:   nitrokeyapp/ui/mainwindow.ui
    	modified:   nitrokeyapp/ui/welcome_tab.ui
    	modified:   nitrokeyapp/welcome_tab.py

--- nitrokeyapp/welcome_tab.py
+++ nitrokeyapp/welcome_tab.py
@@ -1,16 +1,15 @@
+import webbrowser
 from typing import Optional
+from urllib.request import urlopen
 
+from pynitrokey.nk3.utils import Version
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWidget
 
 from nitrokeyapp import __version__
 from nitrokeyapp.logger import save_log
-
-# from pynitrokey.nk3.utils import Version
 from nitrokeyapp.qt_utils_mix_in import QtUtilsMixIn
 from nitrokeyapp.ui.welcome_tab import Ui_WelcomeTab
-
-# from urllib.request import urlopen
 
 
 class WelcomeTab(QtUtilsMixIn, QWidget):
@@ -24,18 +23,28 @@ class WelcomeTab(QtUtilsMixIn, QWidget):
         self.ui.setupUi(self)
         self.ui.buttonSaveLog.pressed.connect(self.save_log)
         self.ui.VersionNr.setText(__version__)
+        self.ui.CheckUpdate.pressed.connect(self.check_update)
 
-    #   self.ui.CheckUpdate.pressed.connect(self.check_nversion)
+    def check_update(self) -> None:
+        self.c_version = __version__
+        self.n_version = urlopen(
+            "https://raw.githubusercontent.com/Nitrokey/nitrokey-app2/main/nitrokeyapp/VERSION"
+        ).read()
+        self.n_version = str(self.n_version, encoding="utf-8")
+        self.n_version = self.n_version[:-1]
 
-    # def check_nversion(self) -> None:
-    #     self.nversion = urlopen('https://raw.githubusercontent.com/Nitrokey/nitrokey-app2/main/nitrokeyapp/VERSION').read()
-    #     self.nversion = str(self.nversion, encoding='utf-8')
-    #     self.nversion = (self.nversion[:-1])
-    #     self.check_update(self)
-    #     print (self.nversion)
-    #     print (type(self.nversion))
+        self.c_version_v = Version.from_str(self.c_version)
+        self.n_version_v = Version.from_str(self.n_version)
 
-    # def check_update(self) -> None:
+        if Version.__lt__(self.c_version_v, self.n_version_v):
+            self.ui.CheckUpdate.setText("update available")
+            self.ui.CheckUpdate.pressed.connect(
+                lambda: webbrowser.open(
+                    "https://github.com/Nitrokey/nitrokey-app2/releases"
+                )
+            )
+        else:
+            self.ui.CheckUpdate.setText("App is up to date")
 
     @pyqtSlot()
     def save_log(self) -> None:
