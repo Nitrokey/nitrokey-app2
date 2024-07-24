98cd4af0e761b8708eff6e1b3505c1f2ce3bc842
Robin Krahl <robin@nitrokey.com>
2024-07-24 09:48:18 +0200

    Remove pynitrokey dependency
    All relevant components of pynitrokey have been moved to the Nitrokey
    Python SDK or can be replaced by the fido2 library.  The last missing
    piece, Retries, is so simple that we can just copy it into this package.
    So we can now remove the pynitrokey dependency.

--- nitrokeyapp/update.py
+++ nitrokeyapp/update.py
@@ -18,7 +18,6 @@ from nitrokey.nk3 import list as list_nk3
 from nitrokey.nk3 import open as open_nk3
 from nitrokey.nk3.updates import Updater, UpdateUi
 from nitrokey.trussed import TrussedBase, Variant, Version
-from pynitrokey.helpers import Retries
 from PySide6.QtCore import QCoreApplication
 
 if TYPE_CHECKING:
@@ -235,3 +234,38 @@ class Nk3Context:
                 self.await_device,
             )
             updater.update(device, image, version, ignore_pynitrokey_version)
+
+
+class Try:
+    """Utility class for an execution of a repeated action with Retries."""
+
+    def __init__(self, i: int, retries: int) -> None:
+        self.i = i
+        self.retries = retries
+
+    def __str__(self) -> str:
+        return f"try {self.i + 1} of {self.retries}"
+
+    def __repr__(self) -> str:
+        return f"Try(i={self.i}, retries={self.retries})"
+
+
+class Retries:
+    """Utility class for repeating an action multiple times until it succeeds."""
+
+    def __init__(self, retries: int, timeout: float = 0.5) -> None:
+        self.retries = retries
+        self.i = 0
+        self.timeout = timeout
+
+    def __iter__(self) -> "Retries":
+        return self
+
+    def __next__(self) -> Try:
+        if self.i >= self.retries:
+            raise StopIteration
+        if self.i > 0:
+            sleep(self.timeout)
+        t = Try(self.i, self.retries)
+        self.i += 1
+        return t
